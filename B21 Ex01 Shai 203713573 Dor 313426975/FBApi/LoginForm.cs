0ed867f1acf1823c41883e1f6aa0adbe759126c1
shaitk5 <54707146+shaitk5@users.noreply.github.com>
2021-03-16 21:17:58 +0200

    Add files via upload

---
+++ B21 Ex01 Shai 203713573 Dor 313426975/FBApi/LoginForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper;
+using Facebook;
+using FacebookApiApp;
+
+namespace FacebookApiApp
+{
+     public partial class LoginForm : Form
+     {
+          LoginResult m_LoginResult;
+          bool m_RememberUser;
+
+          public LoginForm()
+          {
+               InitializeComponent();
+               
+               LoggedInUserData();
+          }
+
+          private void LoggedInUserData()
+          {
+               
+
+          }
+
+          private void LoginButtonOnClick(object sender, EventArgs e)
+          {
+               m_RememberUser = checkBox1.Checked;
+               m_LoginResult = FacebookService.Login("283238313148583", "user_birthday", "user_friends", "user_likes", "user_posts", "public_profile");
+          }
+
+     }
+}
