e40e05c701423d793e3570604d26c24c0118017a
shaitk5 <srsrssr@gmail.com>
2021-03-21 16:02:08 +0200

    arrgghhhhhh

--- B21 Ex01 Shai 203713573 Dor 313426975/FacebookApiApp/LoginForm.Designer.cs
+++ B21 Ex01 Shai 203713573 Dor 313426975/FacebookApiApp/LoginForm.Designer.cs
@@ -28,79 +28,81 @@
           /// </summary>
           private void InitializeComponent()
           {
-            this.checkBoxRemeberMe = new System.Windows.Forms.CheckBox();
-            this.LogoPicture = new System.Windows.Forms.PictureBox();
-            this.LoginButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.LogoPicture)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // checkBoxRemeberMe
-            // 
-            this.checkBoxRemeberMe.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBoxRemeberMe.AutoSize = true;
-            this.checkBoxRemeberMe.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.checkBoxRemeberMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxRemeberMe.Location = new System.Drawing.Point(149, 331);
-            this.checkBoxRemeberMe.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.checkBoxRemeberMe.Name = "checkBoxRemeberMe";
-            this.checkBoxRemeberMe.Size = new System.Drawing.Size(122, 21);
-            this.checkBoxRemeberMe.TabIndex = 2;
-            this.checkBoxRemeberMe.Text = "Remember Me!";
-            this.checkBoxRemeberMe.UseVisualStyleBackColor = true;
-            this.checkBoxRemeberMe.CheckedChanged += new System.EventHandler(this.checkBoxRemeberMe_CheckedChanged);
-            // 
-            // LogoPicture
-            // 
-            this.LogoPicture.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.LogoPicture.BackColor = System.Drawing.Color.White;
-            this.LogoPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.LogoPicture.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.LogoPicture.Image = global::FacebookApiApp.Properties.Resources.FacebookLogo;
-            this.LogoPicture.Location = new System.Drawing.Point(121, 41);
-            this.LogoPicture.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.LogoPicture.Name = "LogoPicture";
-            this.LogoPicture.Size = new System.Drawing.Size(171, 168);
-            this.LogoPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.LogoPicture.TabIndex = 1;
-            this.LogoPicture.TabStop = false;
-            // 
-            // LoginButton
-            // 
-            this.LoginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.LoginButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.LoginButton.BackgroundImage = global::FacebookApiApp.Properties.Resources.FacebookLoginButton;
-            this.LoginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.LoginButton.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoginButton.Location = new System.Drawing.Point(104, 274);
-            this.LoginButton.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(201, 47);
-            this.LoginButton.TabIndex = 0;
-            this.LoginButton.UseVisualStyleBackColor = false;
-            this.LoginButton.Click += new System.EventHandler(this.LoginButtonOnClick);
-            // 
-            // LoginForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(422, 409);
-            this.Controls.Add(this.checkBoxRemeberMe);
-            this.Controls.Add(this.LogoPicture);
-            this.Controls.Add(this.LoginButton);
-            this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.MaximumSize = new System.Drawing.Size(444, 466);
-            this.MinimumSize = new System.Drawing.Size(248, 392);
-            this.Name = "LoginForm";
-            this.Text = "FacebookAPI ";
-            ((System.ComponentModel.ISupportInitialize)(this.LogoPicture)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+               this.checkBoxRemeberMe = new System.Windows.Forms.CheckBox();
+               this.LogoPicture = new System.Windows.Forms.PictureBox();
+               this.LoginButton = new System.Windows.Forms.Button();
+               ((System.ComponentModel.ISupportInitialize)(this.LogoPicture)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // checkBoxRemeberMe
+               // 
+               this.checkBoxRemeberMe.Anchor = System.Windows.Forms.AnchorStyles.None;
+               this.checkBoxRemeberMe.AutoSize = true;
+               this.checkBoxRemeberMe.Cursor = System.Windows.Forms.Cursors.Arrow;
+               this.checkBoxRemeberMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.checkBoxRemeberMe.Location = new System.Drawing.Point(149, 331);
+               this.checkBoxRemeberMe.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+               this.checkBoxRemeberMe.Name = "checkBoxRemeberMe";
+               this.checkBoxRemeberMe.Size = new System.Drawing.Size(122, 21);
+               this.checkBoxRemeberMe.TabIndex = 2;
+               this.checkBoxRemeberMe.Text = "Remember Me!";
+               this.checkBoxRemeberMe.UseVisualStyleBackColor = true;
+               this.checkBoxRemeberMe.CheckedChanged += new System.EventHandler(this.checkBoxRemeberMe_CheckedChanged);
+               // 
+               // LogoPicture
+               // 
+               this.LogoPicture.Anchor = System.Windows.Forms.AnchorStyles.None;
+               this.LogoPicture.BackColor = System.Drawing.Color.White;
+               this.LogoPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+               this.LogoPicture.Cursor = System.Windows.Forms.Cursors.Arrow;
+               this.LogoPicture.Image = global::FacebookApiApp.Properties.Resources.FacebookLogo;
+               this.LogoPicture.Location = new System.Drawing.Point(121, 41);
+               this.LogoPicture.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+               this.LogoPicture.Name = "LogoPicture";
+               this.LogoPicture.Size = new System.Drawing.Size(171, 168);
+               this.LogoPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+               this.LogoPicture.TabIndex = 1;
+               this.LogoPicture.TabStop = false;
+               // 
+               // LoginButton
+               // 
+               this.LoginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+               this.LoginButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.LoginButton.BackgroundImage = global::FacebookApiApp.Properties.Resources.FacebookLoginButton;
+               this.LoginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+               this.LoginButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+               this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.LoginButton.Location = new System.Drawing.Point(104, 274);
+               this.LoginButton.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+               this.LoginButton.Name = "LoginButton";
+               this.LoginButton.Size = new System.Drawing.Size(201, 47);
+               this.LoginButton.TabIndex = 0;
+               this.LoginButton.UseVisualStyleBackColor = false;
+               this.LoginButton.Click += new System.EventHandler(this.LoginButtonOnClick);
+               // 
+               // LoginForm
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+               this.AutoSize = true;
+               this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+               this.BackColor = System.Drawing.Color.White;
+               this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+               this.ClientSize = new System.Drawing.Size(416, 387);
+               this.Controls.Add(this.checkBoxRemeberMe);
+               this.Controls.Add(this.LogoPicture);
+               this.Controls.Add(this.LoginButton);
+               this.Cursor = System.Windows.Forms.Cursors.Default;
+               this.ImeMode = System.Windows.Forms.ImeMode.Off;
+               this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+               this.MaximumSize = new System.Drawing.Size(437, 444);
+               this.MinimumSize = new System.Drawing.Size(241, 370);
+               this.Name = "LoginForm";
+               this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+               this.Text = "Login ";
+               ((System.ComponentModel.ISupportInitialize)(this.LogoPicture)).EndInit();
+               this.ResumeLayout(false);
+               this.PerformLayout();
 
           }
 
--- B21 Ex01 Shai 203713573 Dor 313426975/FacebookApiApp/LoginForm.cs
+++ B21 Ex01 Shai 203713573 Dor 313426975/FacebookApiApp/LoginForm.cs
@@ -7,66 +7,61 @@ namespace FacebookApiApp
 {
      public partial class LoginForm : Form
      {
-        public User LoggedInUser
-        {
-            get;
-            private set;
-        }
-        public LoginResult FormLoginResult
-        {
-            get;
-            private set;
-        }
+          public User LoggedInUser
+          {
+               get;
+               private set;
+          }
+          public LoginResult FormLoginResult
+          {
+               get;
+               private set;
+          }
 
-        public bool RememberMe
-        {
-            get
-            {
-                bool v_IfRemember = true;
+          public bool RememberMe
+          {
+               get;
+               set;
+          }
 
-                v_IfRemember = checkBoxRemeberMe.Checked;
-
-                return v_IfRemember;
-            }
-        }
-
-        public LoginForm()
-        {
+          public LoginForm()
+          {
                InitializeComponent();
-        }
+          }
 
-        private void LoginButtonOnClick(object sender, EventArgs e)
-        {
-            LoggedInUserData();
-        }
+          private void LoginButtonOnClick(object sender, EventArgs e)
+          {
+               LoggedInUserData();
+          }
 
-        private void LoggedInUserData()
-        {
-            FormLoginResult = FacebookService.Login(
-                "283238313148583",
-                "user_birthday",
-                "user_friends",
-                "user_likes",
-                "user_posts",
-                "public_profile");
+          private void LoggedInUserData()
+          {
+               FormLoginResult = FacebookService.Login(
+                   "283238313148583",
+                   "user_birthday",
+                   "user_friends",
+                   "user_likes",
+                   "user_gender",
+                   "user_posts",
+                   "public_profile");
 
-            AppSettings appSettings = AppSettings.LoadFromFile();
+               AppSettings appSettings = AppSettings.LoadFromFile();
 
-            if (!String.IsNullOrEmpty(FormLoginResult.AccessToken))
-            {
-                LoggedInUser = FormLoginResult.LoggedInUser;
-                this.DialogResult = DialogResult.OK;
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show(FormLoginResult.ErrorMessage);
-            }
-        }
+               if(!String.IsNullOrEmpty(FormLoginResult.AccessToken))
+               {
+                    LoggedInUser = FormLoginResult.LoggedInUser;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+               }
+               else
+               {
+                    MessageBox.Show(FormLoginResult.ErrorMessage);
+               }
+          }
 
-        private void checkBoxRemeberMe_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-    }
+          private void checkBoxRemeberMe_CheckedChanged(object sender, EventArgs e)
+          {
+               this.RememberMe = checkBoxRemeberMe.Checked;
+          }
+     }
 }
